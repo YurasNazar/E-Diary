b6a3b256850160595bc8e5219038793dc93e5cdf
Nazar Yuras <yuras.nazar.aandriiovych@gmail.com>
2020-05-10 16:54:34 +0300

    Intstalling bootstrap, jquery. Configure _ViewStart.cshtml, _Layout.cshtml, _ViewImports.cshtml

--- EDiary/EDiary/Startup.cs
+++ EDiary/EDiary/Startup.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +9,19 @@ namespace EDiary
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        // Notice we are using Dependency Injection here
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,6 +32,7 @@ namespace EDiary
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
