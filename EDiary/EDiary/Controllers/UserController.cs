cb8421851ef8e033d13ba5d3c3f6c1b69c852ed9
Nazar Yuras <yuras.nazar.aandriiovych@gmail.com>
2020-05-16 14:40:01 +0300

    Create basic layout page with navigation and footer. Implement login page and functionality. Install font-awesome

--- EDiary/EDiary/Controllers/UserController.cs
+++ EDiary/EDiary/Controllers/UserController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using EDiary.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace EDiary.Controllers
 {
